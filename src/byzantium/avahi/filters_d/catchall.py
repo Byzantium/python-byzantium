376179c354812638733fda3c780539b71ed73da7
haxwithaxe <me@haxwithaxe.net>
2014-01-28 19:53:39 -0500

    added comments

--- src/byzantium/avahi/filters_d/catchall.py
+++ src/byzantium/avahi/filters_d/catchall.py
@@ -5,11 +5,14 @@ from byzantium.avahi.filter import AvahiFilter
 import re
 
 class Filter(AvahiFilter):
-    '''Match all byzantium service types'''
+    """ Match all byzantium service types """
     byz_tcp = re.compile('[\w.-]*__byz__\._tcp')
     byz_udp = re.compile('[\w.-]*__byz__\._udp')
     def match(self, record, action=None):
-        '''match every record for a byzantium service'''
+        """ Match every record for a byzantium service
+        @param  record      Record object to inspect
+        @param  action      action to take based on match
+        """
         if byz_tcp.match(record.service_type) or byz_tcp.match(record.service_type):
             return True
         else:
